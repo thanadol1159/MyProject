623968e3996e83c53ef548bcc9732370f09e95c7
thanadol1159 <87893349+thanadol1159@users.noreply.github.com>
2022-12-19 07:00:03 +0700

    Add files via upload

--- paginateManagement.js
+++ paginateManagement.js
@@ -1,59 +1,80 @@
-// const { template } = require('@babel/core')
-import { getItemsOfCurrentPage, getTotalPages } from './lib/paginate.js'
-import { products } from './data/products.js'
+const { template } = require('@babel/core')
+// import { getItemsOfCurrentPage, getTotalPages } from './lib/paginate.js'
+// import { products } from './data/products.js'
 
-// const { getItemsOfCurrentPage, getTotalPages } = require('./lib/paginate.js')
-// const products = require('./data/products.js')
+const { getItemsOfCurrentPage, getTotalPages } = require('./lib/paginate.js')
+const products = require('./data/products.js')
 
 //your student id, firstname, and lastname here
-// 64130500035 thanadol saengsawang
+//64130500066 Phutawan Palakavong 
 function paginateManagement(items, rows) {
   const rowsPerPage = rows
   const products = items
 
   const showItemsOfCurrentPage = (currentPageNumber) => {
-    const itemsOfCurrentPage = getItemsOfCurrentPage(
-      products,
-      currentPageNumber,
-      rowsPerPage
-    )
-    const productsUl = document.getElementById('products')
-    itemsOfCurrentPage.forEach((item) => {
-      const liElement = document.createElement('li')
-      liElement.textContent = `ID:${item.id}, NAME:${item.name}`
-      productsUl.appendChild(liElement)
-    })
-  }
+    let items = getItemsOfCurrentPage(products,currentPageNumber,rowsPerPage)
+    const ulElement = document.querySelector('#app').querySelector('div').querySelector('ul')
+    // const ulElement = document.body.children[0].children[1].children[0]
+    // console.log(ulElement);
 
-  const pageHandler = (event) => {
-    // clean button click styles
-    const pagesStyle = document.querySelectorAll('button')
-    pagesStyle.forEach((page) => (page.style = 'border: 1px solid #999'))
-
-    //clean previous page's items
-    const productsUl = document.getElementById('products')
-    productsUl.textContent = ''
-
-    //calculating current page
-    const currentPage = event?.target.id ?? 1
-    showItemsOfCurrentPage(currentPage, rowsPerPage)
-
-    //add button click style to a current page
-    const targetPageButton = document.getElementById(currentPage)
-    targetPageButton.style = 'background-color: LightSteelBlue'
-  }
-
-  //generate page numbers and add click event on each of page number
-  const showPageNumbers = () => {
-    const totalPages = getTotalPages(products, rowsPerPage)
-    const paginateDiv = document.querySelector('.pagination')
-    for (let page = 1; page <= totalPages; page++) {
-      const buttonElement = document.createElement('button')
-      buttonElement.textContent = page
-      buttonElement.setAttribute('id', page)
-      paginateDiv.appendChild(buttonElement)
-      buttonElement.addEventListener('click', pageHandler)
+    while (ulElement.firstChild) {
+      ulElement.removeChild(ulElement.firstChild)
     }
+    items.forEach(e => {
+      const newLiElement = document.createElement('li')
+      newLiElement.textContent = `ID:${e.id}, NAME:${e.name}`
+      ulElement.appendChild(newLiElement)
+    })
+    
+    
+  }
+  const pageHandler = (event) => {
+
+    if(event === undefined){
+      showItemsOfCurrentPage(1)
+      const parentElement = document.querySelector('div')
+      const paginationElement = parentElement.children[2]
+      const firstPageButton = paginationElement.children[0]
+      firstPageButton.style = 'background-color: LightSteelBlue'
+    }
+    
+    const pageBtn = event?.target === undefined ? document.getElementById('app').children[2].firstElementChild : event.target;
+    console.log(pageBtn);
+    
+    const pageId = pageBtn === undefined ? 1 : pageBtn.id
+    showItemsOfCurrentPage(pageId)
+    
+    const parentBtn = document.getElementById('app').children[2]
+    console.log(parentBtn);
+    for(let i = 0; i< parentBtn.children.length; i++){
+      const otherPage = parentBtn.children[i]
+      otherPage.style = 'border: 1px solid #999'
+    }
+    pageBtn.style ='background-color: LightSteelBlue'
+    
+
+  }
+  const showPageNumbers = () => {
+    let total = getTotalPages(products, rowsPerPage)
+    // const parentElement = document.body.children[0]
+    const parentElement = document.querySelector('div')
+    // console.log(parentElement);
+    const paginationElement = parentElement.children[2]
+    // console.log(paginationElement);
+    
+
+    for(let i = 1; i<= total; i++){
+      const pageButtonElement = document.createElement('button')
+      pageButtonElement.textContent = i
+      pageButtonElement.setAttribute('id', i)
+      paginationElement.appendChild(pageButtonElement)
+      pageButtonElement.addEventListener('click', pageHandler)
+    }
+
+
+    
+    
+    
   }
 
   return {
@@ -61,8 +82,19 @@ function paginateManagement(items, rows) {
     pageHandler
   }
 }
-// module.exports = paginateManagement
-export { paginateManagement }
-const { showPageNumbers, pageHandler } = paginateManagement(products, 10)
-showPageNumbers()
-pageHandler()
+module.exports = paginateManagement
+// export { paginateManagement }
+// const { showPageNumbers, pageHandler } = paginateManagement(products, 10)
+// showPageNumbers()
+// pageHandler()
+
+
+// best
+// // const { template } = require('@babel/core')
+// import { getItemsOfCurrentPage, getTotalPages } from './lib/paginate.js'
+// import { products } from './data/products.js'
+
+// // const { getItemsOfCurrentPage, getTotalPages } = require('./lib/paginate.js')
+// // const products = require('./data/products.js')
+
+// //your student id, firstname, and lastname here
