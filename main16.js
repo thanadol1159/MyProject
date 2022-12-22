aea9ca9daaaf4b61619d9987e2c84a972af44254
thanadol1159 <87893349+thanadol1159@users.noreply.github.com>
2022-12-23 06:42:26 +0700

    Update main16.js

--- main16.js
+++ main16.js
@@ -94,3 +94,15 @@ const p = document.getElementById('display')
 input.addEventListener('input',()=>{
     p.textContent =  input.value
 })
+
+const createBtn = document.getElementById('submit')
+createBtn.addEventListener('click', (event) => {
+  event.preventDefault()
+  const allInputEles=document.querySelectorAll('input')
+  const isValidInput = Array.from(allInputEles).every(
+    (inputEle) => inputEle.value.length !== 0  )
+  const pEle = document.querySelector('p')
+  if (isValidInput) {
+    pEle.textContent = 'your account has been created!'    pEle.style = 'color:green'  } else {
+    pEle.textContent = 'missing some values, please try again'    pEle.style = 'color:red'  }
+})
