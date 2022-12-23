955e7074711731723380270095c6cde979836c0e
thanadol1159 <87893349+thanadol1159@users.noreply.github.com>
2022-12-23 10:58:51 +0700

    Update itemList.js

--- test-filter-items-PhuMiZz-main/test-filter-items-PhuMiZz-main/itemList.js
+++ test-filter-items-PhuMiZz-main/test-filter-items-PhuMiZz-main/itemList.js
@@ -1,48 +1,55 @@
-// import { products } from './data/products.js'
 const { template } = require('@babel/core')
 const products = require('./data/products.js')
-
+// import { products } from './data/products.js'
 //your student id, firstname, and lastname here
-// 64130500079 Siripoom Kusonsong
+//64130500035 Thanadol saengsawng
 function itemList(userItems) {
-  const items = userItems
+  const items = userItems  
 
   const initialPage = () => {
-    const inputElement = document.querySelector("input");
-    inputElement.addEventListener("input", filterItemsHandler
-    )
-    showItems(items)
+    const div = document.getElementById('categories')
+    div.addEventListener('click',showItems(products))
+
   }
 
-  const filterItemsHandler = (event) => {
-    const inputKey = event.target.value
-    let filterItem = items.filter((product) =>
-      product.keywords.toLowerCase().includes(inputKey.toLowerCase()))
-    console.log(filterItem.length);
-    showItems(filterItem)
+  const filterItemsByCategoryHandler = (event) => {
+    const  ul = document.getElementById('items')
+    const div = document.getElementById('categories')
+    const btnsty =document.querySelectorAll('button')
+    btnsty.forEach((color) => {color.style = 'background-color:lightgray'
+  
+    
+  }) 
 
+    const current = event?.target.id ?? 1
 
+    const colortarget = document.getElementById(current)
+    colortarget.style = `background-color:yellow`
+
+    const filteredItems = products.filter((product) => {
+      return product.category.toLowerCase() === btnsty.id.toLowerCase()
+    })
+    showItems(filteredItems)
 
   }
 
   const showItems = (items) => {
-    const ulElement = document.getElementById("items")
-    ulElement.textContent = ""
-    for (const product of items) {
-      const liElement = document.createElement("li")
-      liElement.textContent = `ID:${product.id}, NAME:${product.name}, KEYWORDS:${product.keywords}`
-      ulElement.appendChild(liElement)
+    const  ul = document.getElementById('items')
+    for (let i = 0; i < items.length; i++) {
+      const li = document.createElement('li')
+      li.textContent=`ID:${products[i].id}, NAME:${products[i].name}, CATEGORY:${products[i].category}`
+      ul.appendChild(li)
     }
-
   }
 
   return {
     initialPage,
-    filterItemsHandler,
+    filterItemsByCategoryHandler,
     showItems
   }
 }
 module.exports = itemList
 // export { itemList }
-// const { initialPage, filterItemsHandler, showItems } = itemList(products)
+// const { initialPage, filterItemsByCategoryHandler, showItems } =
+//   itemList(products)
 // initialPage()
